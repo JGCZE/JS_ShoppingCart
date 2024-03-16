4504e30930b6f30019eb926c2fc5d2445aab1db1
Jan Gross <jan.gross.cze@gmail.com>
2024-03-16 16:29:12 +0100

    Function for maping products. Just 4 for testing

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
-let shop = document.getElementById('shop');
+let shop = document.getElementById("shop");
+
+let shopItemsData = [
+  {
+    id: "1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "lorem impsum dolor sit amet conceoneoe",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "2",
+    name: "Office Shirt",
+    price: 450,
+    desc: "lorem impsum dolor sit amet conceoneoe",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "3",
+    name: "T Shirt",
+    price: 5,
+    desc: "lorem impsum dolor sit amet conceoneoe",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "4",
+    name: "Mens suit",
+    price: 300,
+    desc: "lorem impsum dolor sit amet conceoneoe",
+    img: "images/img-4.jpg",
+  },
+];
 
 let generateShop = () => {
-  return shop.innerHTML = `
-      <div class="item">
-      <img width="220" src="./images/img-1.jpg" alt="">
-      <div class="details">
-        <h3>Casual shirt</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. atum! Rem, optio.</p>
-        <div class="price-quantity">
-          <h2>$45</h2>
-          <div class="buttons">
-            <i class="bi bi-dash-lg"></i>
-            <div class="quantity">0</div>
-            <i class="bi bi-plus-lg"></i>
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      const { id, name, price, desc, img } = x
+      return `
+        <div class="item" id=product-id-${id} >
+            <img width="220" src=${img} alt="">
+            <div class="details">
+              <h3>${name}</h3>
+              <p>${desc}</p>
+              <div class="price-quantity">
+                <h2>$ ${price}</h2>
+                <div class="buttons">
+                  <i class="bi bi-dash-lg"></i>
+                  <div class="quantity" id=${id}>0</div>
+                  <i class="bi bi-plus-lg"></i>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-  `
-}
+        `;
+    })
+    .join(""));
+};
 
-generateShop()
+generateShop();
